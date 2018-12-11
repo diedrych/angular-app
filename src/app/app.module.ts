3dffd52936830a9050220a2c5f66884a2ed4b528
solange jimenez <dsja-angel@hotmail.com>
2018-12-11 11:53:51 -0500

    form angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from "./material/material.module";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './shared/table/table.component';
+import { EmployeesComponent } from './shared/employees/employees.component';
+import { EmployeeComponent } from './shared/employees/employee/employee.component';
+import {EmployeeService} from './shared/employee.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    EmployeesComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
